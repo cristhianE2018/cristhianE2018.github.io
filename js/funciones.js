b77064e5a716a6f0a382efa3a825820f64befc01
cristhian2019 <cristhian@idp.com>
2020-06-12 23:53:40 -0500

    Navbar, footer y animaciones establecidos

--- js/funciones.js
+++ js/funciones.js
@@ -2,3 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
   });
+
+let animado = document.querySelectorAll(".animado");
+
+function mostrarScroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  for(var i=0; i<animado.length; i++){
+    let alturaAnimado = animado[i].offsetTop;
+    if(alturaAnimado - 350 < scrollTop){
+      animado[i].style.opacity = 1;
+      animado[i].classList.add("mostrarArriba");
+    }
+  }
+}
+
+window.addEventListener('scroll',mostrarScroll);
